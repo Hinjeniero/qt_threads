fe1ffb9aafa31ee89a54ecd67e52d05fb2dd6a16
Hinjeniero <dflaityp@alumnos.unex.es>
2017-09-21 23:03:35 +0200

    Delete unnecesary files

--- ejemplo_thread_class/CMakeFiles/ejemplo_thread_class.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ejemplo_thread_class.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- ejemplo_thread_class/CMakeFiles/ejemplo_thread_class.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ejemplo_thread_class.
-# This may be replaced when dependencies are built.
